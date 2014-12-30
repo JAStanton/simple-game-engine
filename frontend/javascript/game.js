8fc222553b8157efffa5550be80a09fb011f7665
Jonathan Stanton <jonathan@jastanton.com>
2014-12-30 14:54:48 -0800

    proper walls

--- frontend/javascript/game.js
+++ frontend/javascript/game.js
@@ -14,46 +14,22 @@ goog.require('game.core.helper');
 game.Main = function() {
   game.Main.base(this, 'constructor');
 
+  var width = 500;
+  var height = 300;
+
   /** @private {!game.core.Entity} */
   this.gameBoard_ = new game.core.Entity().
       addClass('game-board').
       mixin('shape').
-      setRectangle(new game.core.math.Vector(), 500, 300).
+      setRectangle(new game.core.math.Vector(), width, height).
       attach(this.root);
 
   this.gameBoard_.init();
 
-  var wall0 = new game.core.Entity().
-      addClass('boundary').
-      mixin('shape', 'physical').
-      setRectangle(new game.core.math.Vector(0, 0), 2, 300).
-      attach(this.gameBoard_);
-  wall0.init();
-  wall0.registerCollider('wall-0', game.core.Entity);
-
-  var wall1 = new game.core.Entity().
-      addClass('boundary').
-      mixin('shape', 'physical').
-      setRectangle(new game.core.math.Vector(0, 0), 500, 2).
-      attach(this.gameBoard_);
-  wall1.init();
-  wall1.registerCollider('wall-1', game.core.Entity);
-
-  var wall2 = new game.core.Entity().
-      addClass('boundary').
-      mixin('shape', 'physical').
-      setRectangle(new game.core.math.Vector(0, 300), 2, 500).
-      attach(this.gameBoard_);
-  wall2.init();
-  wall2.registerCollider('wall-2', game.core.Entity);
-
-  var wall3 = new game.core.Entity().
-      addClass('boundary').
-      mixin('shape', 'physical').
-      setRectangle(new game.core.math.Vector(500, 0), 2, 300).
-      attach(this.gameBoard_);
-  wall3.init();
-  wall3.registerCollider('wall-3', game.core.Entity);
+  this.addPlatform('wall-0', 0, 0, width, 5);
+  this.addPlatform('wall-1', 0, 0, 5, height);
+  this.addPlatform('wall-2', height - 5, 0, width, 5);
+  this.addPlatform('wall-3', 0, width - 5, 5, height);
 
   /** @private {!game.core.Entity} */
   this.player_ = new game.core.Entity().
@@ -69,12 +45,31 @@ game.Main = function() {
       registerCollidesWith('wall-3');
 
 
-
   /** @private {!game.core.Entity} */
   this.camera_ = new game.core.Camera(this.gameBoard_).
       addLayer(this.gameBoard_).
       watch(this.player_);
 };
 game.core.helper.inherit(game.Main, game.core.Main);
+
+
+/**
+ * Adds a platform
+ *
+ * @param {string} wallName
+ * @param {number} top
+ * @param {number} left
+ * @param {number} width
+ * @param {number} height
+ */
+game.Main.prototype.addPlatform = function(wallName, top, left, width, height) {
+  var wall = new game.core.Entity().
+      addClass('boundary').
+      mixin('shape', 'physical').
+      setRectangle(new game.core.math.Vector(left, top), width, height).
+      attach(this.gameBoard_);
+  wall.init();
+  wall.registerCollider(wallName, game.core.Entity);
+};
 
 var main = new game.Main();
